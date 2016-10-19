9a7ef1ee92e32955e8386fdbcffe61a74f31f9d2
anzizhao <anzizhao@126.com>
2016-10-19 19:38:20 +0800

    保存修改内容

--- modules/run/index.js
+++ modules/run/index.js
@@ -16,19 +16,19 @@ exports = module.exports = function (scraper, rssReader, jsonFetcher, config) {
         collectAndDistributeContent: function (callback) {
             async.parallel({
                 runSiteScraper: function (callback) {
-                    async.forever(scraper.run.bind(scraper), callback);
+                    //async.forever(scraper.run.bind(scraper), callback);
                 },
                 runRssFeedParser: function (callback) {
                     //async.forever(rssReader.run.bind(rssReader), callback);
-                    setTimeout(function(){
-                        async.forever(rssReader.run.bind(rssReader), callback);
-                    },  10 * 60 * 1000 ) // 开启后10分钟抓取
+                    //setTimeout(function(){
+                        //async.forever(rssReader.run.bind(rssReader), callback);
+                    //},  10 * 60 * 1000 ) // 开启后10分钟抓取
                 },
                 runJsonFetcherAndMapper: function (callback) {
-                    //async.forever(jsonFetcher.run.bind(jsonFetcher), callback);
-                    setTimeout(function(){
-                        async.forever(jsonFetcher.run.bind(jsonFetcher), callback);
-                    },  30 * 60 * 1000 ) // 30分钟抓取
+                    async.forever(jsonFetcher.run.bind(jsonFetcher), callback);
+                    //setTimeout(function(){
+                        //async.forever(jsonFetcher.run.bind(jsonFetcher), callback);
+                    //},  30 * 60 * 1000 ) // 30分钟抓取
                 }
             }, callback);
         }
